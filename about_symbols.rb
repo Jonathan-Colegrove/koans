1552b506f96fe6d3fbba35c3a72c8363093f62cd
Jonathan Colegrove <Demetrius472@gmail.com>
2016-08-01 19:15:34 -0400

    finished 8

--- about_symbols.rb
+++ about_symbols.rb
@@ -44,20 +44,20 @@ class AboutSymbols < Neo::Koan
 
   def test_symbols_can_be_made_from_strings
     string = "catsAndDogs"
-    assert_equal __, string.to_sym
+    assert_equal :catsAndDogs, string.to_sym
   end
 
   def test_symbols_with_spaces_can_be_built
     symbol = :"cats and dogs"
 
-    assert_equal __.to_sym, symbol
+    assert_equal "cats and dogs".to_sym, symbol
   end
 
   def test_symbols_with_interpolation_can_be_built
     value = "and"
     symbol = :"cats #{value} dogs"
 
-    assert_equal __.to_sym, symbol
+    assert_equal "cats and dogs".to_sym, symbol
   end
 
   def test_to_s_is_called_on_interpolated_symbols
